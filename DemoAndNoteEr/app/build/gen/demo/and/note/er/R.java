a8e9b61a457e51e2d768280c8e697ddae56f2583
lgsgithub13 <m18658756225@163.com>
2019-01-18 19:19:00 +0800

    为主活动添加菜单

--- DemoAndNoteEr/app/build/gen/demo/and/note/er/R.java
+++ DemoAndNoteEr/app/build/gen/demo/and/note/er/R.java
@@ -14,15 +14,23 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int mainButton1=0x7f060000;
-        public static final int mainButton2=0x7f060001;
+        public static final int item1=0x7f070002;
+        public static final int item2=0x7f070003;
+        public static final int mainButton1=0x7f070000;
+        public static final int mainButton2=0x7f070001;
     }
     public static final class layout {
         public static final int main=0x7f030000;
     }
+    public static final class menu {
+        public static final int main_menu=0x7f060000;
+    }
     public static final class string {
         public static final int app_name=0x7f040000;
-        public static final int hello_world=0x7f040001;
+        public static final int main_activity_1=0x7f040003;
+        public static final int main_activity_2=0x7f040004;
+        public static final int main_menu_1=0x7f040001;
+        public static final int main_menu_2=0x7f040002;
     }
     public static final class style {
         /** 	按钮颜色<item name="android:colorButtonNormal">#ff009688</item>
--- DemoAndNoteEr/app/src/main/java/demo/and/note/er/MainActivity.java
+++ DemoAndNoteEr/app/src/main/java/demo/and/note/er/MainActivity.java
@@ -2,6 +2,8 @@ package demo.and.note.er;
 
 import android.app.*;
 import android.os.*;
+import android.view.*;
+import android.widget.*;
 
 public class MainActivity extends Activity 
 {
@@ -11,4 +13,28 @@ public class MainActivity extends Activity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
     }
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu)
+	{
+		MenuInflater mi=getMenuInflater();
+		mi.inflate(R.menu.main_menu,menu);
+		return true;
+	}
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item)
+	{
+		switch(item.getItemId())
+		{
+			case R.id.item1:
+				Toast.makeText(MainActivity.this,R.string.main_activity_1,Toast.LENGTH_LONG).show();
+				return true;
+			case R.id.item2:
+				new AlertDialog.Builder(this)
+				.setTitle(R.string.main_menu_2)
+				.setMessage(R.string.main_activity_2)
+				.show();
+				return true;
+		}
+		return true;
+	}
 }
